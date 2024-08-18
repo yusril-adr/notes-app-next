ed51e5ed0092b717034db7f46174ad2613e34c0d
Yusril Adriansyah Putra <yusriladr.37@gmail.com>
2024-08-18 15:42:58 +0700

    Refactor(layout) remove unused variables

--- src/app/layout/Global.tsx
+++ src/app/layout/Global.tsx
@@ -2,12 +2,11 @@
 
 import AuthenticationError from "@errors/AuthenticationError";
 import { useAlert } from "@hooks/alert";
-import { useAppDispatch, useAppSelector } from "@hooks/redux";
+import { useAppDispatch } from "@hooks/redux";
 import TokenService from "@services/localstorage/TokenService";
 import { loginByToken } from "@states/auth";
 import CONFIG from "@utils/contants/config";
 import { AxiosError } from "axios";
-import { usePathname, useRouter } from "next/navigation";
 import { FC, ReactNode, useEffect } from "react";
 
 const GlobalLayout: FC<{
@@ -15,36 +14,34 @@ const GlobalLayout: FC<{
 }> = ({ children }) => {
   const dispatch = useAppDispatch();
   const { showAlertError } = useAlert();
-  const pathname = usePathname();
-  const router = useRouter();
-
-  const loginHandler = async () => {
-    const resultAction = await dispatch(loginByToken());
-
-    if (!loginByToken.fulfilled.match(resultAction)) {
-      const payload = resultAction.payload;
-      if (payload instanceof AxiosError) {
-        let error = resultAction.payload;
-
-        if (payload.response?.status === 401) {
-          error = new AuthenticationError("Log In session expired.");
-        }
-
-        showAlertError(error);
-      } else {
-        throw resultAction.error;
-      }
-    }
-  };
 
   useEffect(() => {
     if (
       TokenService.getToken(CONFIG.NOTES_API_ACCESS_KEY) &&
       TokenService.getToken(CONFIG.NOTES_API_REFRESH_KEY)
     ) {
+      const loginHandler = async () => {
+        const resultAction = await dispatch(loginByToken());
+
+        if (!loginByToken.fulfilled.match(resultAction)) {
+          const payload = resultAction.payload;
+          if (payload instanceof AxiosError) {
+            let error = resultAction.payload;
+
+            if (payload.response?.status === 401) {
+              error = new AuthenticationError("Log In session expired.");
+            }
+
+            showAlertError(error);
+          } else {
+            throw resultAction.error;
+          }
+        }
+      };
+
       loginHandler();
     }
-  }, []);
+  }, [dispatch, showAlertError]);
 
   return <>{children}</>;
 };
